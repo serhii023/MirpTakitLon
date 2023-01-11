59b9192044025b60d0b979ab5c859766a45c0d78
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-01-11 23:04:10 +0200

    add main_page beauty and init RandomLetters urls

--- MirplMapTok/MirplMapTok/settings.py
+++ MirplMapTok/MirplMapTok/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MainPage.apps.MainpageConfig',
+    'RandomLetters.apps.RandomlettersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
