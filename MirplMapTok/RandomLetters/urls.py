1336486bd896d9d5a55f6e9dcd0a4dab53ac0836
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-01-13 10:41:13 +0200

    Create RandomLetters interface

--- MirplMapTok/RandomLetters/urls.py
+++ MirplMapTok/RandomLetters/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('print-random-leters/', views.random_index, name='random_index'),
+
+    # print-random-leters/?size_from=50&size_to=70
 ]
--- MirplMapTok/RandomLetters/views.py
+++ MirplMapTok/RandomLetters/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
 
+# from .forms import NameForm
+
+
 def index(request):
-    return render(request, 'RandomLetters/app_wrapper.html')
+    return render(request,
+                  'RandomLetters/app_wrapper.html',
+                  {'size_from': 50, 'size_to': 70, 'amount': 10})
+
+def random_index(request):
+    if request.method == 'GET':
+        size_from = request.GET.get('size_from')
+        size_to = request.GET.get('size_to')
+        amount = request.GET.get('amount')
+        random_list = []
+        for i in range(amount):
+            random_list.append()
+    else:
+        print("Unespected error")
+
+    return render(request,
+                  'RandomLetters/app_get_random_leters.html',
+                  {'size_from': size_from, 'size_to': size_to, 'amount': amount})
+
+
+
